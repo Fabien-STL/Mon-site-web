754e9528511b6664172ba26b0df648b5c964c391
Fabien-STL <79516609+Fabien-STL@users.noreply.github.com>
2021-03-09 15:42:03 +0400

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-//fichier de script pour faire apparaitre le menu de navigation
-
-const liste = document.querySelector('.liste');
-const btnResponsive = document.querySelector('.btn-respondive');
-
-btnResponsive.addEventListener('click',() => {
-    liste.classList.toggle('show');
-})
